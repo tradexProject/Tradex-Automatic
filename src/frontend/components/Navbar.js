f0e59721efa8cfbd0b45f4a1b4d2c17427fdc387
tradexProject <automatictradex@gmail.com>
2026-05-10 00:28:48 +0300

    Update Navbar.js

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { Activity } from 'lucide-react';
 
 export default function Navbar() {
   const scrollTo = (id) => {
@@ -9,33 +8,26 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 w-full z-50 backdrop-blur-xl border-b border-white/10 ">
+    <nav className="fixed top-0 w-full z-50 backdrop-blur-xl border-b border-white/10">
       <div className="max-w-7xl mx-auto px-6 lg:px-8 py-4 flex items-center">
-        
+        <div className="flex-1 flex justify-start">
+          <div onClick={() => scrollTo('home')} className="flex items-center gap-3 cursor-pointer group">
+                        <div className="relative flex items-center justify-center transition-transform duration-500 group-hover:scale-105">
+              <img 
+                src="/logo.png"  
+                alt="Tradex Logo" 
+                className="w-10 h-10 object-contain relative z-10" 
+              />
+            </div>
 
-<div className="flex-1 flex justify-start">
-  <div onClick={() => scrollTo('home')} className="flex items-center cursor-pointer group">
-    <div className="relative flex items-center justify-center w-10 h-10 transition-transform duration-500 group-hover:scale-105">
-      
-      <div className="absolute inset-0 from-indigo-500 to-cyan-400 rounded-xl opacity-70 blur-md group-hover:opacity-100  duration-500"></div>
-      
-      <div className="absolute inset-0  from-indigo-500 to-cyan-400 rounded-xl shadow-inner "></div>
-      
-      <img 
-        src="/logo.png"  
-        alt="Tradex Logo" 
-        className="w-7 h-7 object-contain relative z-10" 
-      />
-    </div>
-
-    <span className="text-white/90 font-semibold tracking-wide text-lg group-hover:text-white transition-colors duration-300">
-  Trade
-  <span className="bg-gradient-to-tr from-indigo-400 to-cyan-400 bg-clip-text text-transparent">
-    X
-  </span>
-</span>
-  </div>
-</div>
+            <span className="text-white/90 font-bold tracking-tight text-2xl group-hover:text-white transition-colors duration-300">
+              Trade
+              <span className="bg-gradient-to-tr from-indigo-400 to-cyan-400 bg-clip-text text-transparent">
+                X
+              </span>
+            </span>
+          </div>
+        </div>
         
         {/* Links Section - Center */}
         <div className="hidden md:flex items-center justify-center gap-2">
